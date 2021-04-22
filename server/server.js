e12bb536123aa2dd617a57d4e86f194e68a61ac4
shubham-pal-au9 <spal5776@gmail.com>
2021-04-22 12:38:08 +0530

    Added Api

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const cors = require('cors');
 const mongourl = `mongodb://localhost:27017`;
 let dbobj;
 let col_name="TV"
+let col_mobile="Mobile"
+let col_laptop="Laptop"
+let col_watch="Watch"
 
 
 //middleware
@@ -23,8 +26,8 @@ app.use(bodyParser.json())
 app.get('/',(req,res) => {
     res.status(200).send("Health Ok")
 })
-
-//postUser
+//--------------------------------TV Api-------------------------//
+//post TV 
 app.post('/addProducts',(req,res)=>{
     dbobj.collection(col_name).insert(req.body,(err,result) => {
         if(err) throw err;
@@ -33,7 +36,7 @@ app.post('/addProducts',(req,res)=>{
 });
 
 
-//getUser
+//get TV 
 app.get('/allProducts',(req,res) => {
     dbobj.collection(col_name).find().toArray((err,result) => {
         if(err) throw err;
@@ -42,7 +45,7 @@ app.get('/allProducts',(req,res) => {
 })
 
 
-//updateUser
+//update TV 
 app.put('/updateProducts/:_id',(req,res) => {
     let Id = req.params._id;
     dbobj.collection(col_name).update(
@@ -63,7 +66,7 @@ app.put('/updateProducts/:_id',(req,res) => {
     )
 })
 
-//DeleteUser
+//Delete
 app.delete('/deleteProduct/:_id',(req,res) => {
     let Id = mongo.ObjectID(req.params._id);
     dbobj.collection(col_name).remove({_id:Id},(err,result) => {
@@ -71,6 +74,120 @@ app.delete('/deleteProduct/:_id',(req,res) => {
         res.send("Data Deleted")
     })
 });
+
+//----------------------Mobile Api----------------------------------//
+
+//get Mobile
+app.get('/allMobiles',(req,res) => {
+    dbobj.collection(col_mobile).find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//post TV 
+app.post('/addMobile',(req,res)=>{
+    dbobj.collection(col_mobile).insert(req.body,(err,result) => {
+        if(err) throw err;
+        res.status(200).send("Data Added")
+    })
+});
+
+//update TV 
+app.put('/updateMobile/:_id',(req,res) => {
+    let Id = req.params._id;
+    dbobj.collection(col_mobile).update(
+        {_id:mongo.ObjectID(Id)},
+        {
+            $set:{
+                name:req.body.name,
+                os:req.body.os,
+                ram: req.body.ram,
+                memory: req.body.memory,
+                price:req.body.price,
+            }
+        },(err,result) => {
+            if(err) throw err;
+            res.send("Data Updated")
+        }
+    )
+})
+
+//----------------------Laptop Api----------------------------------//
+
+//get Mobile
+app.get('/allLaptop',(req,res) => {
+    dbobj.collection(col_laptop).find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//post TV 
+app.post('/addLaptop',(req,res)=>{
+    dbobj.collection(col_laptop).insert(req.body,(err,result) => {
+        if(err) throw err;
+        res.status(200).send("Data Added")
+    })
+});
+
+//update TV 
+app.put('/updateLaptop/:_id',(req,res) => {
+    let Id = req.params._id;
+    dbobj.collection(col_laptop).update(
+        {_id:mongo.ObjectID(Id)},
+        {
+            $set:{
+                name:req.body.name,
+                os:req.body.os,
+                ram: req.body.ram,
+                memory: req.body.memory,
+                price:req.body.price,
+            }
+        },(err,result) => {
+            if(err) throw err;
+            res.send("Data Updated")
+        }
+    )
+})
+
+//----------------------Watches Api----------------------------------//
+
+//get Mobile
+app.get('/allWatches',(req,res) => {
+    dbobj.collection(col_watch).find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//post TV 
+app.post('/addWatch',(req,res)=>{
+    dbobj.collection(col_watch).insert(req.body,(err,result) => {
+        if(err) throw err;
+        res.status(200).send("Data Added")
+    })
+});
+
+//update TV 
+app.put('/updateWatch/:_id',(req,res) => {
+    let Id = req.params._id;
+    dbobj.collection(col_watch).update(
+        {_id:mongo.ObjectID(Id)},
+        {
+            $set:{
+                name:req.body.name,
+                os:req.body.os,
+                memory: req.body.memory,
+                price:req.body.price,
+            }
+        },(err,result) => {
+            if(err) throw err;
+            res.send("Data Updated")
+        }
+    )
+})
+
 
 
 //connecting database to nodejs
